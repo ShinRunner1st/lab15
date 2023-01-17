fa5a201b6dcdc22d90556cf7b60ddffbd7f44643
ShinRunner1st <shinrunner1st@gmail.com>
2023-01-17 19:26:35 +0700

    F

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    for (int i = 1; i < N; i++)
+    {
+        T x = d[i];
+        int j = i - 1;
+        while (x > d[j] && j >= 0)
+        {
+            d[j + 1] = d[j];
+            --j;
+        }
+    d[j + 1] = x;
+    cout << "Pass " << i << ":";
+    for(int i = 0; i < N; i++) cout << d[i] << " ";
+    cout << endl;
+    }
 }
 
 int main(){
